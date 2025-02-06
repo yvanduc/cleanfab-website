cf2fd64cae5e2d7bfa90751e1fd0ab3e3d8755c3
yvanduc <yvanduc.video@gmail.com>
2025-02-06 14:49:32 +0100

    Ajustement des périodes avec tranches de 5 ans pour les véhicules récents

--- 03_Development/Database/periodes_vehicules.sql
+++ 03_Development/Database/periodes_vehicules.sql
@@ -1,8 +1,10 @@
 -- Insertion des périodes dans l'ordre décroissant
 INSERT INTO cf_periodes_vehicules (annee_debut, annee_fin, description, is_oldtimer) VALUES
-(2020, 2029, 'Années 2020', FALSE),
-(2010, 2019, 'Années 2010', FALSE),
-(2000, 2009, 'Années 2000', FALSE),
+(2021, 2025, 'Années 2021-2025', FALSE),
+(2016, 2020, 'Années 2016-2020', FALSE),
+(2011, 2015, 'Années 2011-2015', FALSE),
+(2006, 2010, 'Années 2006-2010', FALSE),
+(2000, 2005, 'Années 2000-2005', FALSE),
 (1990, 1999, 'Années 90', FALSE),
 (1980, 1989, 'Années 80', TRUE),
 (1970, 1979, 'Années 70', TRUE),
@@ -12,6 +14,6 @@ INSERT INTO cf_periodes_vehicules (annee_debut, annee_fin, description, is_oldti
 (1930, 1939, 'Années 30', TRUE);
 
 -- Requête pour l'interface utilisateur (à utiliser dans le code)
-SELECT id, description 
+SELECT id, description, is_oldtimer
 FROM cf_periodes_vehicules 
 ORDER BY annee_debut DESC;
